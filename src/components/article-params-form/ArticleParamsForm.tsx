4767d57113c319ed9d8bf59f93fbc7f9647b1bce
art0tod <todorartem@icloud.com>
2024-11-29 17:50:59 +0300

    marked up menu content

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,12 +1,17 @@
 import { useState, useRef, useEffect } from 'react';
+import { defaultArticleState, fontFamilyOptions, fontSizeOptions, fontColors, backgroundColors, contentWidthArr } from 'src/constants/articleProps'
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
+import { Text } from 'src/ui/text';
+import { Select } from 'src/ui/select'
+import { RadioGroup } from 'src/ui/radio-group'
+import { Separator } from 'src/ui/separator'
 
 import styles from './ArticleParamsForm.module.scss';
 
 export const ArticleParamsForm = () => {
 	const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
-	const sidebarRef = useRef<HTMLElement | null>(null);
+	const menuRef = useRef<HTMLElement | null>(null);
 
 	const handleMenuToggle = () => {
 		setIsMenuOpen(prev => !prev);
@@ -14,7 +19,7 @@ export const ArticleParamsForm = () => {
 
 	useEffect(() => {
 		const handleClickOutside = (evt: MouseEvent) => {
-			if (sidebarRef.current && !sidebarRef.current.contains(evt.target as Node)) {
+			if (menuRef.current && !menuRef.current.contains(evt.target as Node)) {
 				setIsMenuOpen(false);
 			}
 		};
@@ -29,10 +34,61 @@ export const ArticleParamsForm = () => {
 		<>
 			<ArrowButton isOpen={isMenuOpen} onClick={handleMenuToggle} />
 			<aside
-				ref={sidebarRef}
+				ref={menuRef}
 				className={`${styles.container} ${isMenuOpen ? styles.container_open : ''}`}
 			>
 				<form className={styles.form}>
+					<Text
+						as='h2'
+						size={31}
+						weight={800}
+						fontStyle='normal'
+						uppercase
+						align='left'
+						family='open-sans'
+					>
+						Задайте параметры
+					</Text>
+					<Select
+						selected={defaultArticleState.fontFamilyOption}
+						options={fontFamilyOptions}
+						placeholder='Выберите шрифт'
+						onChange={() => { }}
+						onClose={() => { }}
+						title='Шрифт'
+					/>
+					<RadioGroup
+						name='fontSize'
+						selected={defaultArticleState.fontSizeOption}
+						options={fontSizeOptions}
+						onChange={() => { }}
+						title='Размер шрифта'
+					/>
+					<Select
+						selected={defaultArticleState.fontColor}
+						options={fontColors}
+						placeholder='Выберите цвет шрифта'
+						onChange={() => { }}
+						onClose={() => { }}
+						title='Цвет шрифта'
+					/>
+					<Separator />
+					<Select
+						selected={defaultArticleState.backgroundColor}
+						options={backgroundColors}
+						placeholder='Выберите цвет фона'
+						onChange={() => { }}
+						onClose={() => { }}
+						title='Цвет фона'
+					/>
+					<Select
+						selected={defaultArticleState.contentWidth}
+						options={contentWidthArr}
+						placeholder='Выберите ширину контента'
+						onChange={() => { }}
+						onClose={() => { }}
+						title='Ширина контента'
+					/>
 					<div className={styles.bottomContainer}>
 						<Button title="Сбросить" htmlType="reset" type="clear" />
 						<Button title="Применить" htmlType="submit" type="apply" />
